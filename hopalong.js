c1ea57b79bcf0e5a1c6b623b72615a0ac24b8330
Louis <louis@lpil.co.uk>
2014-11-14 18:37:59 +0000

    Working hopalong

--- hopalong.js
+++ hopalong.js
@@ -1,40 +1,27 @@
-// (function() {
+(function() {
   var i,
       canvas = document.querySelector('canvas'),
       ctx    = canvas.getContext('2d'),
-      width  = 500,
-      height = 500,
-      canvasData,
-      hopalongX,
-      hopalongY,
-      a = 114,
-      b = 110.9,
-      c = 1111,
-      x = 0,
-      y = 0;
+      width  = 1200,
+      height = 1200,
+      a  = 0.4,
+      b  = 0.1,
+      c  = 0.3,
+      x  = 0,
+      y  = 0,
+      xx = 0,
+      yy = 0;
   
-  hopalongX = function hopalongX(x, y) {
-    var signX = x > 0 ? 1 : -1,
-        z = signX * Math.sqrt(Math.abs(b * x - c));
-
-    if (x > 0) { return y - z; }
-    else       { return y + z; }
-  };
-
-  hopalongY = function hopalongY(x) {
-    return a - x;
-  };
-
-
   canvas.width  = width;
   canvas.height = height,
-  canvasData = ctx.getImageData(0, 0, width, height);
 
-  i = 10000;
+  i = 350000;
   while (i--) {
-    x = hopalongX(x, y);
-    y = hopalongY(x);
-    console.log(x + width / 2, y + height / 2);
-    ctx.fillRect(x + width / 2, y + height / 2, 1, 1);
+    ctx.fillRect(x*130 + width / 2, y*130 + height / 2, 1, 1);
+
+    xx = y - (x > 0 ? 1 : -1) * Math.sqrt(Math.abs(b * x - c));
+    yy = a - x;
+    x  = xx;
+    y  = yy;
   }
-// }());
+}());
