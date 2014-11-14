fd894b15efa783a556cd7903e0b957ffd3375f54
Louis Pilfold <louis@lpil.uk>
2014-11-14 12:01:45 +0000

    Move getting frequency data into func

--- rolks-legacy.js
+++ rolks-legacy.js
@@ -5,7 +5,12 @@
       source       = audioContext.createMediaElementSource(player),
       bufferLength = analyser.frequencyBinCount,
       dataArray    = new Uint8Array(bufferLength),
-      average;
+      getBands,
+      average,
+      rolk;
+
+  window.rolk = window.rolk || {};
+  rolk = window.rolk;
 
   average = function average(array) {
     var i = array.length,
@@ -15,6 +20,21 @@
       sum = sum + array[i];
     }
     return sum / array.length;
+  };
+
+
+  getBands = function getBands(numBands) {
+    var i, l,
+        band,
+        chunk = Math.floor(dataArray.length / numBands),
+        bands = [];
+    analyser.getByteFrequencyData(dataArray);
+
+    for (i = 0, l = dataArray.length; i < l; i += chunk) {
+      band = average(dataArray.subarray(i, i + chunk));
+      bands.push(Math.floor(band));
+    }
+    return bands;
   };
 
 
@@ -31,20 +51,7 @@
   // Transform config
   analyser.fftSize = 2048;
 
-
-  // Perform FFT on audio, split into 8 bands, print values
-  setInterval(function() {
-    var i,
-        l,
-        chunk = 64,
-        bands = [];
-
-    analyser.getByteFrequencyData(dataArray);
-
-    for (i = 0, l = dataArray.length; i < l; i += chunk) {
-      bands.push(Math.floor(average(dataArray.subarray(i, i + chunk))));
-    }
-    console.log(bands);
-  }, 100 );
+  // Perform FFT on audio, split into bands, print values
+  setInterval(function() { console.log(getBands(16)); }, 100 );
 
 }());
