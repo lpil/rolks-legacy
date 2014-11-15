027b1477f32e712d75e721210de631baa7715709
Louis <louis@lpil.co.uk>
2014-11-15 01:13:02 +0000

    Remove redundant assignment, rename: i -> points

--- hopalong.js
+++ hopalong.js
@@ -1,22 +1,21 @@
 (function() {
-  var i,
-      canvas = document.querySelector('canvas'),
+  var canvas = document.querySelector('canvas'),
       ctx    = canvas.getContext('2d'),
       width  = 1200,
       height = 1200,
+      points = 350000,
       a  = 0.4,
       b  = 0.1,
       c  = 0.3,
       x  = 0,
       y  = 0,
-      xx = 0,
-      yy = 0;
+      xx,
+      yy;
   
   canvas.width  = width;
   canvas.height = height,
 
-  i = 350000;
-  while (i--) {
+  while (points--) {
     ctx.fillRect(x*130 + width / 2, y*130 + height / 2, 1, 1);
 
     xx = y - (x > 0 ? 1 : -1) * Math.sqrt(Math.abs(b * x - c));
